8ee0ed12a63d2061b06913c69037a737474acb2f
titan00001 <gplay9792@gmail.com>
2019-12-13 22:41:59 +0530

    fixed and refactored judge

--- asset/scripts/judge.js
+++ asset/scripts/judge.js
@@ -1,21 +1,22 @@
-// conf is configuration of playground
-// conf = [0,-1,0, 1,1,-1, -1,-1,-1] =>    0  -1   0
+// configuration is configurationiguration of playground
+// configuration = [0,-1,0, 1,1,-1, -1,-1,-1] =>    0  -1   0
                                         //     1   1  -1
                                         //    -1  -1  -1
 
-const w = [[0,1,2],
+const winningConfiguration = [[0,1,2],
 [3,4,5],
 [6,7,8],
 [0,3,6],
 [1,4,7],
 [2,5,8],
 [0,4,8],
-[2,4,6]]
-function isComplete(conf){
+[2,4,6]];
+
+function isComplete(configuration){
 
     let i = 0;
     while(i < 9){
-        if(conf[i] == -1){
+        if(configuration[i] == -1){
             return false;
         }
 
@@ -27,15 +28,15 @@ function isComplete(conf){
 
 // -1 = not completed 0 = o won 1 = x won
 
-export function judge(conf){
+export function judge(configuration){
 
 
-    for(let i of w){
+    for(let i of winningConfiguration){
         
         let p = i[0], q = i[1], r = i[2];
 
-        if((conf[p] === conf[q]) && (conf[p] === conf[r]) && (conf[p] !== -1)){
-            return conf[p];
+        if((configuration[p] === configuration[q]) && (configuration[p] === configuration[r]) && (configuration[p] !== -1)){
+            return configuration[p];
         }
 
     }
