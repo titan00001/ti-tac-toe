7b1fb936dea910d19c52ecc281092abcf4473e14
titan00001 <gplay9792@gmail.com>
2019-12-18 21:40:05 +0530

    deploy on port provided or 3000 if not available

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require('express');
 
 const app = express();
 
+PORT = process.env.port | 3000;
+
+
 app.use(express.static('asset'));
 
 app.get('/', function(req, res) {
     res.sendFile("./asset/index.html");
 })
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("Server running on port 3000");
 })
